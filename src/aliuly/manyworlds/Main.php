4a29de00f7974138297e3049af89fb6376ffed4d
YTiStrafeNubzHD <31699109+YTiStrafeNubzHD@users.noreply.github.com>
2017-10-15 18:51:27 +0200

    Update Main.php

--- src/aliuly/manyworlds/Main.php
+++ src/aliuly/manyworlds/Main.php
@@ -72,7 +72,7 @@ class Main extends BasicPlugin implements CommandExecutor {
 	// Command dispatcher
 	//
 	//////////////////////////////////////////////////////////////////////
-	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 		if ($cmd->getName() != "manyworlds") return false;
 		return $this->dispatchSCmd($sender,$cmd,$args);
 	}
